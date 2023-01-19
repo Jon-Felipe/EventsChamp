5719e9e21c4fada397134d0d150e3df6296a1d68
Jon-Felipe <rodriguesjf7@gmail.com>
2023-01-19 20:09:40 +0000

    Created basic getEvent createEvent updateEvent deleteEvent routes

--- controllers/events.js
+++ controllers/events.js
@@ -5,6 +5,38 @@ const getAllEvents = async (req, res) => {
   res.send('get all events');
 };
 
+// @desc    Fetch single event
+// @route   GET /api/v1/events/:id
+// @access  Public
+const getEvent = async (req, res) => {
+  res.send('get event');
+};
+
+// @desc    Create event
+// @route   POST /api/v1/events
+// @access  Private
+const createEvent = async (req, res) => {
+  res.send('create event');
+};
+
+// @desc    Update Event
+// @route   PATCH /api/v1/events/:id
+// @access  Private
+const updateEvent = async (req, res) => {
+  res.send('update event');
+};
+
+// @desc    Delete Event
+// @route   DELETE /api/v1/events/:id
+// @access  Private
+const deleteEvent = async (req, res) => {
+  res.send('delete event');
+};
+
 module.exports = {
   getAllEvents,
+  getEvent,
+  createEvent,
+  updateEvent,
+  deleteEvent,
 };
