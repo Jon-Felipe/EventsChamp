bf64bcc607c8dfe95b321c1cb5981566895d91f8
Jon-Felipe <rodriguesjf7@gmail.com>
2023-01-20 20:34:36 +0000

    Removed Tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div>
       <Header />
-      <h1>Hello world</h1>
     </div>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+// extra
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h1>header</h1>
+    </div>
+  );
 };
 
 export default Header;
