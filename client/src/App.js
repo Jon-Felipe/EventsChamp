a7d0b4b820a989705f73ffd0950e2cb26ec21714
Jon-Felipe <rodriguesjf7@gmail.com>
2023-01-22 12:57:46 +0000

    Removed Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // components
 import Header from './components/Header';
-import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Navbar />
     </Router>
   );
 };
