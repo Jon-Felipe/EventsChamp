8c61a16151c6e3bbee66893c16b7872d07769d85
Jon-Felipe <rodriguesjf7@gmail.com>
2023-01-28 11:38:37 +0000

    Added event images and initialized Hero component

---
+++ client/src/components/Layout/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+const Hero = () => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
