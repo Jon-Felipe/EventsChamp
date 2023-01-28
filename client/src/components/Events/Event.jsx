b8632b1db9023ee42ca32ff5b6719d2f2426d93b
Jon-Felipe <rodriguesjf7@gmail.com>
2023-01-28 10:18:07 +0000

    Update event card styling

--- client/src/components/Events/Event.jsx
+++ client/src/components/Events/Event.jsx
@@ -1,59 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaHeart, FaRegHeart, FaArrowRight } from 'react-icons/fa';
-
-// components
-import Card from '../UI/Card';
-
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
 // extras
 import eventImg from '../../assets/dj.jpg';
 
-const Event = ({ id, title, location, date, venue, price, isFavourite }) => {
+const Event = ({ id, title, location, date, price, isFavourite }) => {
   return (
-    <Card>
-      <div className='relative'>
-        <img src={eventImg} alt='dj deck' />
-        <div className='flex items-center p-4 gap-x-2'>
-          <div className='basis-1/6 text-center'>
-            <p className='text-violet-500 font-bold text-lg sm:text-base lg:text-lg'>
-              {date.split(' ')[1]}
-            </p>
-            <p className='font-bold text-lg sm:text-base lg:text-lg'>
-              {date.split(' ')[0]}
-            </p>
-          </div>
-          <div className='basis-5/6'>
-            <h4 className='font-bold text-lg sm:text-sm lg:text-base'>
-              {title}
-            </h4>
-            <p className='text-slate-500 sm:text-sm'>{location}</p>
-            <p className='text-slate-500 sm:text-sm'>{venue}</p>
-          </div>
-          <div className='absolute top-4 left-0 px-4 w-full flex items-center justify-between'>
-            <p className='bg-white rounded-md px-4 py-2 font-bold'>
-              ${price.toFixed(2)}
-            </p>
-            <div className='flex items-center'>
-              {isFavourite ? (
-                <div className='bg-white rounded-full p-3 hover:scale-125 ease-in-out duration-300 cursor-pointer'>
-                  <FaHeart size='1.3rem' className='text-red-500' />
-                </div>
-              ) : (
-                <div className='bg-white rounded-full p-3 hover:scale-125 ease-in-out duration-300 cursor-pointer'>
-                  <FaRegHeart size='1.3rem' />
-                </div>
-              )}
-              <Link
-                to={`/event/${id}`}
-                className='bg-white rounded-full p-3 ml-3 hover:scale-125 ease-in-out duration-300'
-              >
-                <FaArrowRight size='1.3rem' />
-              </Link>
-            </div>
+    <div className='relative'>
+      <Link to={`/event/${id}`}>
+        <div className='grid grid-rows-[300px_1fr]'>
+          <img
+            src={eventImg}
+            alt={title}
+            className='w-full h-full object-cover rounded-xl'
+          />
+          <div className='mt-3'>
+            <h3 className='font-bold text-lg'>{title}</h3>
+            <p>{location}</p>
+            <p>{date}</p>
+            <p className='font-bold'>${price.toFixed(2)}</p>
           </div>
         </div>
+      </Link>
+      <div className='absolute top-4 right-4'>
+        {isFavourite ? (
+          <div className='bg-white rounded-full p-3 hover:scale-125 ease-in-out duration-300 cursor-pointer'>
+            <FaHeart size='1.3rem' className='text-red-500' />
+          </div>
+        ) : (
+          <div className='bg-white rounded-full p-3 hover:scale-125 ease-in-out duration-300 cursor-pointer'>
+            <FaRegHeart size='1.3rem' />
+          </div>
+        )}
       </div>
-    </Card>
+    </div>
   );
 };
 
--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -12,7 +12,7 @@ const HomeScreen = () => {
     <section className='p-4'>
       {/* Events filter */}
       <article className='max-w-6xl mx-auto sm:flex sm:items-center sm:justify-between'>
-        <h3 className='text-4xl font-bold text-center sm:text-2xl md:text-3xl'>
+        <h3 className='text-xl font-bold text-center md:text-3xl'>
           Upcoming Events
         </h3>
         <div className='mt-8 grid grid-cols-3 gap-x-2 sm:mt-0 md:gap-x-4'>
@@ -22,14 +22,13 @@ const HomeScreen = () => {
         </div>
       </article>
       {/* Event Cards */}
-      <article className='mt-8 max-w-6xl mx-auto grid gap-4 sm:grid-cols-2 sm:gap-8 lg:grid-cols-3'>
+      <article className='mt-8 max-w-7xl mx-auto grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
         {events.map((event) => (
           <Event
             key={event.id}
             id={event.id}
             title={event.title}
             location={event.location}
-            venue={event.venue}
             date={event.date}
             price={event.price}
             isFavourite={event.isFavourite}
