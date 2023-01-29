9470df22f2459369bcc52ba478724a42fb798515
Jon-Felipe <rodriguesjf7@gmail.com>
2023-01-29 13:28:48 +0000

    Header styling updates

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Header = () => {
   const UserLinks = () => {
     return (
-      <nav className='mt-4 sm:mt-0'>
-        <ul className='grid grid-cols-2 gap-x-4 sm:grid-cols-[150px_150px]'>
-          <li className='text-center bg-white p-2 rounded-md shadow-md'>
+      <nav>
+        <ul className='flex items-center justify-between gap-x-2 lg:gap-x-5'>
+          <li className='py-2 px-6'>
             <Link to='/login' className='text-lg font-medium tracking-wider'>
               Login
             </Link>
           </li>
-          <li className='bg-violet-500 p-2 text-center rounded-md shadow-md'>
+          <li className='bg-violet-200 py-2 px-6 rounded-lg'>
             <Link
               to='/sign-up'
-              className='text-lg text-white font-medium tracking-wide'
+              className='text-lg text-violet-700 font-medium tracking-wider'
             >
               Sign Up
             </Link>
@@ -26,18 +25,11 @@ const Header = () => {
   };
 
   return (
-    <header className='p-4 shadow-sm'>
-      <div className='max-w-7xl mx-auto sm:flex sm:justify-between sm:items-center'>
-        <div className='flex items-center justify-between'>
-          <Link to='/' className='font-bold text-4xl'>
-            EventsChamps
-          </Link>
-          <button className='sm:hidden'>
-            <FaTimes size='2rem' />
-          </button>
-        </div>
-        <UserLinks />
-      </div>
+    <header className='flex items-center justify-between h-20 px-5 max-w-7xl mx-auto xl:px-0'>
+      <Link to='/' className='font-bold text-3xl'>
+        EventsChamps
+      </Link>
+      <UserLinks />
     </header>
   );
 };
