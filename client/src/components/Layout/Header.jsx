048d0ec2e57f9d61d12dc5c0613ff2fd3deecbf8
Jon-Felipe <rodriguesjf7@gmail.com>
2023-02-05 09:18:30 +0000

    Header styling updates

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
               to='/sign-up'
               className='text-lg text-violet-700 font-medium tracking-wider'
             >
-              Sign Up
+              Register
             </Link>
           </li>
         </ul>
@@ -26,7 +26,7 @@ const Header = () => {
 
   return (
     <header className='flex items-center justify-between h-20 px-5 max-w-7xl mx-auto xl:px-0'>
-      <Link to='/' className='font-bold text-3xl'>
+      <Link to='/' className='font-bold text-xl md:text-3xl'>
         EventsChamps
       </Link>
       <UserLinks />
