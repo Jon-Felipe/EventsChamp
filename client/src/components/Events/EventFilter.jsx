aab48d6f869e4e58527111f6d552dbd1d61f3718
Jon-Felipe <rodriguesjf7@gmail.com>
2023-02-05 09:58:26 +0000

    EventFilter styling updates

--- client/src/components/Events/EventFilter.jsx
+++ client/src/components/Events/EventFilter.jsx
@@ -5,16 +5,14 @@ import Select from '../UI/Select';
 
 const EventFilter = () => {
   return (
-    <>
-      <h3 className='text-xl font-bold text-center md:text-3xl'>
-        Upcoming Events
-      </h3>
-      <div className='mt-8 grid grid-cols-3 gap-x-2 sm:mt-0 md:gap-x-4'>
+    <section className='my-5 px-5 max-w-7xl mx-auto md:flex md:items-center md:justify-between 2xl:px-0'>
+      <h3 className='text-3xl font-bold text-center'>Upcoming Events</h3>
+      <div className='mt-5 grid grid-cols-3 gap-x-2 md:mt-0 md:gap-x-4'>
         <Select option='Weekdays' />
         <Select option='Event Type' />
         <Select option='Any Category' />
       </div>
-    </>
+    </section>
   );
 };
 
